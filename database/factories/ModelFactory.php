cf447d2d85f6b45ccace9ce806cf63ac691780cb
Jose Ederango Jr <joseederangojr@gmail.com>
2021-09-26 15:47:22 +0800

    initial commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var  \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Brackets\AdminAuth\Models\AdminUser::class, function (Faker\Generator $faker) {
+    return [
+        'activated' => true,
+        'created_at' => $faker->dateTime,
+        'deleted_at' => null,
+        'email' => $faker->email,
+        'first_name' => $faker->firstName,
+        'forbidden' => $faker->boolean(),
+        'language' => 'en',
+        'last_login_at' => $faker->dateTime,
+        'last_name' => $faker->lastName,
+        'password' => bcrypt($faker->password),
+        'phone_no' => $faker->phoneNumber,
+        'remember_token' => null,
+        'updated_at' => $faker->dateTime,
+
+    ];
+});
---
+++ resources/lang/en/admin.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+    'admin-user' => [
+        'title' => 'Users',
+
+        'actions' => [
+            'index' => 'Users',
+            'create' => 'New User',
+            'edit' => 'Edit :name',
+            'edit_profile' => 'Edit Profile',
+            'edit_password' => 'Edit Password',
+        ],
+
+        'columns' => [
+            'id' => 'ID',
+            'last_login_at' => 'Last login',
+            'activated' => 'Activated',
+            'email' => 'Email',
+            'first_name' => 'First name',
+            'forbidden' => 'Forbidden',
+            'language' => 'Language',
+            'last_name' => 'Last name',
+            'password' => 'Password',
+            'password_repeat' => 'Password Confirmation',
+                
+            //Belongs to many relations
+            'roles' => 'Roles',
+                
+        ],
+    ],
+
+    'admin-user' => [
+        'title' => 'Users',
+
+        'actions' => [
+            'index' => 'Users',
+            'create' => 'New User',
+            'edit' => 'Edit :name',
+            'edit_profile' => 'Edit Profile',
+            'edit_password' => 'Edit Password',
+        ],
+
+        'columns' => [
+            'id' => 'ID',
+            'last_login_at' => 'Last login',
+            'activated' => 'Activated',
+            'email' => 'Email',
+            'first_name' => 'First name',
+            'forbidden' => 'Forbidden',
+            'language' => 'Language',
+            'last_name' => 'Last name',
+            'password' => 'Password',
+            'password_repeat' => 'Password Confirmation',
+            'phone_no' => 'Phone no',
+                
+            //Belongs to many relations
+            'roles' => 'Roles',
+                
+        ],
+    ],
+
+    'list' => [
+        'title' => 'List',
+
+        'actions' => [
+            'index' => 'List',
+            'create' => 'New List',
+            'edit' => 'Edit :name',
+        ],
+
+        'columns' => [
+            'id' => 'ID',
+            
+        ],
+    ],
+
+    // Do not delete me :) I'm used for auto-generation
+];
