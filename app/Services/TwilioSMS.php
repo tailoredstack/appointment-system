e8366c4a9a7957339d05164beb1fe99489f59018
Jose Ederango Jr <joseederangojr@gmail.com>
2021-10-04 02:40:56 +0800

    feat(twilio): add wrapper class for sms messages api

---
+++ app/Services/TwilioSMS.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Config\TwilioConfig;
+use BadMethodCallException;
+use Twilio\Rest\Client;
+
+abstract class TwilioSMS
+{
+    /**
+     * @var \Twilio\Rest\Client;
+     */
+    private $client;
+
+    /**
+     * @var \App\Config\TwilioConfig;
+     */
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = new TwilioConfig(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_SMS_NUMBER'));
+        $this->client = new Client($this->config->sid, $this->config->token);
+    }
+
+    public function __call($method, $args)
+    {
+        if (method_exists($this->client->messages, $method)) {
+            return call_user_func(array($this->client->messages, $method), $args);
+        } else {
+            throw new BadMethodCallException();
+        }
+    }
+}
