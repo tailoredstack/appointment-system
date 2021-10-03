0caf2e18c5806b88e9d47cdebc755ea7f8d243fb
Jose Ederango Jr <joseederangojr@gmail.com>
2021-10-04 04:24:31 +0800

    refactor(twilio): add mesasaging service sid

--- app/Config/TwilioConfig.php
+++ app/Config/TwilioConfig.php
@@ -6,11 +6,12 @@ class TwilioConfig
 {
     private $config = [];
 
-    public function __construct(string $sid, string $token, string $sms_number)
+    public function __construct(string $sid, string $token, string $sms_number, string $service_sid)
     {
         $this->config['sid'] = $sid;
         $this->config['token'] = $token;
         $this->config['sms_number'] = $sms_number;
+        $this->config['service_sid'] = $service_sid;
     }
 
     public function __get($key)
--- app/Services/TwilioSMS.php
+++ app/Services/TwilioSMS.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Config\TwilioConfig;
 use BadMethodCallException;
+use Illuminate\Support\Facades\Log;
 use Twilio\Rest\Client;
 
 abstract class TwilioSMS
@@ -20,24 +21,26 @@ abstract class TwilioSMS
 
     public function __construct()
     {
-        $this->config = new TwilioConfig(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_SMS_NUMBER'));
+        $this->config = new TwilioConfig(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_SMS_NUMBER'), env('TWILIO_SERVICE_SID'));
         $this->client = new Client($this->config->sid, $this->config->token);
     }
 
-    public function __call($method, $args)
-    {
-        if (method_exists($this->client->messages, $method)) {
-            return call_user_func(array($this->client->messages, $method), $args);
-        } else {
-            throw new BadMethodCallException();
-        }
-    }
+    // public function __call($method, $args)
+    // {
+    //     if (method_exists($this->client->messages, $method)) {
+    //         return call_user_func(array($this->client->messages, $method), $args);
+    //     } else {
+    //         throw new BadMethodCallException();
+    //     }
+    // }
 
     public function send(string $to, string $message)
     {
-        $this->create($to, [
+        Log::debug("Sending sms to {$to} {$message} from {$this->config->sms_number} with message {$message}");
+        $this->client->messages->create($to, [
             'from' => $this->config->sms_number,
-            'body' => $message
+            'body' => $message,
+            'messagingServiceSid' => $this->config->service_sid
         ]);
     }
 }
