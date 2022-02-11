0706219adc0f2a1259b59406a6936f6d393e7186
Jose Ederango Jr <35944253+joseederangojr@users.noreply.github.com>
2022-02-11 23:22:18 +0800

    remove sms option (#60)

--- app/Services/TwilioSMS.php
+++ app/Services/TwilioSMS.php
@@ -3,8 +3,6 @@
 namespace App\Services;
 
 use App\Config\TwilioConfig;
-use BadMethodCallException;
-use Illuminate\Support\Facades\Log;
 use Twilio\Rest\Client;
 
 abstract class TwilioSMS
@@ -21,24 +19,13 @@ abstract class TwilioSMS
 
     public function __construct()
     {
-        $this->config = new TwilioConfig(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_SMS_NUMBER'), env('TWILIO_SERVICE_SID'));
+        $this->config = new TwilioConfig(env('TWILIO_SID'), env('TWILIO_TOKEN'), env('TWILIO_SERVICE_SID'));
         $this->client = new Client($this->config->sid, $this->config->token);
     }
 
-    // public function __call($method, $args)
-    // {
-    //     if (method_exists($this->client->messages, $method)) {
-    //         return call_user_func(array($this->client->messages, $method), $args);
-    //     } else {
-    //         throw new BadMethodCallException();
-    //     }
-    // }
-
     public function send(string $to, string $message)
     {
-        Log::debug("Sending sms to {$to} {$message} from {$this->config->sms_number} with message {$message}");
         $this->client->messages->create($to, [
-            'from' => $this->config->sms_number,
             'body' => $message,
             'messagingServiceSid' => $this->config->service_sid
         ]);
