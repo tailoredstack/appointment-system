a7bffddadbead7d7884c26ee8bcf4c85848ca895
Jose Ederango Jr <joseederangojr@gmail.com>
2022-02-11 18:31:24 +0800

    remove sms option

--- app/Config/TwilioConfig.php
+++ app/Config/TwilioConfig.php
@@ -6,11 +6,10 @@ class TwilioConfig
 {
     private $config = [];
 
-    public function __construct(string $sid, string $token, string $sms_number, string $service_sid)
+    public function __construct(string $sid, string $token, string $service_sid)
     {
         $this->config['sid'] = $sid;
         $this->config['token'] = $token;
-        $this->config['sms_number'] = $sms_number;
         $this->config['service_sid'] = $service_sid;
     }
 
